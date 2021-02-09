952930b7a12d868d94a0e7b7ad2debd450b43b2f
Mandy <34908590+picaq@users.noreply.github.com>
2021-02-08 18:08:41 -0800

    create forloops to add random letters into html table

--- boggle.js
+++ boggle.js
@@ -1,20 +1,40 @@
 // source of boggle dice faces 
 // https://boardgames.stackexchange.com/questions/29264/boggle-what-is-the-dice-configuration-for-boggle-in-various-languages?rq=1
-let dice = [
-    ['R', 'I', 'F', 'O', 'B', 'X'],
-    ['I', 'F', 'E', 'H', 'E', 'Y'],
-    ['D', 'E', 'N', 'O', 'W', 'S'],
-    ['U', 'T', 'O', 'K', 'N', 'D'],
-    ['H', 'M', 'S', 'R', 'A', 'O'],
-    ['L', 'U', 'P', 'E', 'T', 'S'],
-    ['A', 'C', 'I', 'T', 'O', 'A'],
-    ['Y', 'L', 'G', 'K', 'U', 'E'],
-    ['Qu', 'B', 'M', 'J', 'O', 'A'],
-    ['E', 'H', 'I', 'S', 'P', 'N'],
-    ['V', 'E', 'T', 'I', 'G', 'N'],
-    ['B', 'A', 'L', 'I', 'Y', 'T'],
-    ['E', 'Z', 'A', 'V', 'N', 'D'],
-    ['R', 'A', 'L', 'E', 'S', 'C'],
-    ['U', 'W', 'I', 'L', 'R', 'G'],
-    ['P', 'A', 'C', 'E', 'M', 'D']
-];
+window.onload = initialize;
+
+function initialize() {
+    rollDice();
+}
+
+function rollDice() {
+    let dice = [
+        ['R', 'I', 'F', 'O', 'B', 'X'],
+        ['I', 'F', 'E', 'H', 'E', 'Y'],
+        ['D', 'E', 'N', 'O', 'W', 'S'],
+        ['U', 'T', 'O', 'K', 'N', 'D'],
+        ['H', 'M', 'S', 'R', 'A', 'O'],
+        ['L', 'U', 'P', 'E', 'T', 'S'],
+        ['A', 'C', 'I', 'T', 'O', 'A'],
+        ['Y', 'L', 'G', 'K', 'U', 'E'],
+        ['Qu', 'B', 'M', 'J', 'O', 'A'],
+        ['E', 'H', 'I', 'S', 'P', 'N'],
+        ['V', 'E', 'T', 'I', 'G', 'N'],
+        ['B', 'A', 'L', 'I', 'Y', 'T'],
+        ['E', 'Z', 'A', 'V', 'N', 'D'],
+        ['R', 'A', 'L', 'E', 'S', 'C'],
+        ['U', 'W', 'I', 'L', 'R', 'G'],
+        ['P', 'A', 'C', 'E', 'M', 'D']
+    ];
+
+    let randomDice = [];    // shuffles dice
+    for ( let i = dice.length ; i > 0 ; i-- ) {
+        let random = Math.floor( Math.random() * i );
+        randomDice.push(...dice.splice(random, 1));
+    }
+
+    let tdArray = document.getElementsByTagName("TD");  // adds letters into td
+    for ( let i = 0 ; i < 16 ; i++ ) {
+        let random = Math.floor( Math.random() * 6 );
+        tdArray[i].innerHTML = randomDice[i][random];
+    }
+}
